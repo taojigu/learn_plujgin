c7b4b1e606367f5786ccb003b074b8ad00a98578
gujitao001 <gujitao001@ke.com>
2019-03-20 16:45:35 +0800

    Add project

---
+++ ios/Classes/LearnPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface LearnPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/LearnPlugin.m
@@ -0,0 +1,20 @@
+#import "LearnPlugin.h"
+
+@implementation LearnPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"learn_plugin"
+            binaryMessenger:[registrar messenger]];
+  LearnPlugin* instance = [[LearnPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
